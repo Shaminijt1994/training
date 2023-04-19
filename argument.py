a3123627ee226411bf0b7cd238815ee38d1c6bf9
Shaminijthampi <Shaminijthampi2023@gmail.com>
2023-04-19 14:31:41 +0530

    modified files

--- argument.py
+++ argument.py
@@ -1,21 +1,17 @@
-def my_var(v1,v2,v3):
-    v1=int(input("v1="))
-    v2=int(input("v2="))
-    v3=int(input("v3="))
-    if(v1 == 1):
-        print("The polygon is a square")
-    else:
-        print("Try Again")
-    
-def myFun(*argv):
-    print("Name is " + str(argv[0]))
-    print("age is " + str(argv[1]))
-    print("city is " + str(argv[2]))
-    print("phone is " + str(argv[3]))
- 
- 
-myFun('30', 'shamu', 'tvm', 'phone')
+import math
 
-#myFun(10) => 100
-#myFun(10, 20) => 200
-#myFun(10, 20, 30) => sdasd
+def find_area(*sides):
+    if len(sides)==1:
+        return sides[0]*sides[0]
+    elif len(sides)==2:
+        return sides[0]*sides[1]
+    elif len(sides)==3:
+        s = (sides[0] + sides[1] + sides[2])/2
+        area = math.sqrt( s * (s-sides[0]) * (s-sides[1]) * (s-sides[2]))  
+        return area  
+    else:
+        return False
+    
+print(find_area(13))
+print(find_area(21, 47))
+print(find_area(10, 20, 12))
